c85ce37436b125501f52938d92f1e9b3794155de
Peter Lock <peterlock@catalyst-au.net>
2024-10-18 06:55:36 +1000

    Issue 43, Issue 50 Tweak Admin Menus

--- version.php
+++ version.php
@@ -24,9 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-
-$plugin->version   = 2024102400; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2024102400;
+$plugin->version   = 2024102800; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2024102800;
 $plugin->requires  = 2020110900; // Requires this Moodle version.
 $plugin->component = 'tool_advancedreplace';
 $plugin->maturity  = MATURITY_STABLE;
